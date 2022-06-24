71efa657f1851b25a4fe6d4482c18b608e5b0f42
Marta <martasagun95@gmail.com>
2022-06-24 12:51:48 +0200

    added function cards render

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,22 @@ const formElem = document.querySelector('.workers_form');
 
 let workersArr = [];
 
+const cardsRender = () => {
+    const cardsContainer = document.querySelector('.cards_container');
+    cardsContainer.innerText = '';
+    workersArr.forEach(({name, lastname, age}) => {
+        const container= document.createElement('div');
+        const nameElem = document.createElement('p');
+        const lastnameElem = document.createElement('p');
+        const ageElem = document.createElement('p');
+        nameElem.innerText = `First  name: ${name}`;
+        lastnameElem.innerText = `Lastname: ${lastname}`;
+        ageElem.innerText = `Age: ${age}`;
+        container.append(nameElem, lastnameElem, ageElem);
+        cardsContainer.append(container);
+    });
+}
+
 formElem.addEventListener('submit', (event) => {
     event.preventDefault();
     const { name, lastname, age } = event.target; //this(если обычная функция)
@@ -13,4 +29,5 @@ formElem.addEventListener('submit', (event) => {
     name.value = '';
     lastname.value = '';
     age.value = '';
+    cardsRender();
 });
